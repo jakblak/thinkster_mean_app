8e76f0f23b5f869accc94d93ecbfb6563193afb5
Patrick Schroeder <schroe9@gmail.com>
2015-05-09 12:05:07 -0700

    initial

---
+++ README.md
@@ -0,0 +1,29 @@
+Flapper News
+mean-stack-tutorial
+=================== 
+
+My code for tuturial provided by https://thinkster.io/angulartutorial/mean-stack-tutorial/ . <br>
+
+I also used bower for frontend package management.
+
+What are MEAN stack components:
+
+M - mongoDB db via mongoose.js library for NodeJs <br>
+E - expressjs backend framework for NodeJs <br>
+A - AngularJs frontend framewrok <br>
+N - NodeJs server technology <br>
+
+It requries nodejs, npm and mongodb installed and running.
+
+
+** Add a Link with a Title
+-- option to comment on and vote up 
+-- vote up both posts and comments
+* in-line templates in public_html/index.html  =  posts.html, home.html
+
+> node app
+
+* Persistent
+-- saves to: mongodb://localhost/news
+
+
---
+++ app.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+//MongoDB connecting and modeling:
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/news');
+require('./models/Posts');
+require('./models/Comments');
+
+
+var routes = require('./routes/index');
+
+
+var app = express();
+app.set('port', process.env.PORT || 3000);
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public_html')));
+
+app.use('/', routes);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Express started on http://localhost:' + 
+        app.get('port') + '; press Ctrl-c to terminate');
+});
+module.exports = app;
